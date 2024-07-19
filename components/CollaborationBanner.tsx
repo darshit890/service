ef52786241246cecf4bf798f12486e5257527d53
darshit890 <88980439+darshit890@users.noreply.github.com>
2024-07-19 16:37:27 +0530

    modified:   components/CollaborationBanner.tsx

--- components/CollaborationBanner.tsx
+++ components/CollaborationBanner.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 const CollaborationBanner: React.FC = () => {
   return (
-    <div className="relative w-full h-[300px] sm:h-[400px] md:h-[500px] overflow-hidden rounded-3xl max-w-7xl mx-auto my-20">
+    <div className="relative w-full  h-[300px] sm:h-[400px] md:h-[500px] overflow-hidden rounded-3xl max-w-7xl mx-auto my-20">
       <Image
         src="/heros.avif" // Replace with your actual image path
         alt="Team Collaboration"
         layout="fill"
         objectFit="cover"
-        className="brightness-75"
+        className="brightness-75 "
       />
       <div className="absolute inset-0 bg-purple-500 bg-opacity-50"></div>
       <div className="absolute inset-0 flex flex-col justify-center px-6 sm:px-12">
